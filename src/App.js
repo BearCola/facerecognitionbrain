41c8a27059e687d3fb1a2f5ab61b8ed691b95b12
sangin <sangin84@gmail.com>
2020-10-04 09:00:41 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+
+const app = new Clarifai.App({
+  apiKey: 'cb1ff9b8f6dd4b0087b6a7a842a56727',
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
+function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [input, setInput] = useState('');
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState('signin');
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  };
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+
+    app.models
+      .predict(
+        // Clarifai.FACE_DETECT_MODEL,
+        'c0c0ac362b03416da06ab3fa36fb58e3',
+        // THE JPG
+        // 'https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp'
+        input
+      )
+      .then((response) => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              setUser({ ...user, entries: Number(count) });
+            });
+        }
+        displayFaceBox(calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const onRouteChange = (route) => {
+    if (route === 'signout') {
+      setIsSignedIn(false);
+    } else if (route === 'home') {
+      setIsSignedIn(true);
+    }
+
+    setRoute(route);
+  };
+
+  const loadUser = (user) => {
+    setUser(user);
+  };
+
+  return (
+    <div className="App">
+      <Particles className="particles" params={particlesOptions} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === 'home' ? (
+        <>
+          <Logo />
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </>
+      ) : route === 'signin' ? (
+        <Signin onRouteChange={onRouteChange} loadUser={loadUser} />
+      ) : (
+        <Register onRouteChange={onRouteChange} loadUser={loadUser} />
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+// cb1ff9b8f6dd4b0087b6a7a842a56727
